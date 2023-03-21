27e4bbeadfd7bcb6e4cd0b0b723761b86737fab1
Daniele Gazziano <daniele.gazziano@iissfermi.edu.it>
2023-03-21 11:34:37 +0100

    aggiornamento file readme

---
+++ cifra D.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    char letter = 'D';
+
+    // Cifra la lettera in un numero usando la tabella ASCII
+    int number = static_cast<int>(letter);
+
+    std::cout << "Lettera originale: " <<lsetter << std::endl>>;
+    std::cout << "Numero cifrato: " << number << std::endl;
+
+    return 0;
+}
